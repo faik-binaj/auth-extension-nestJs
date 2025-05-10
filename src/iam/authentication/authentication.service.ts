9523b7eca0cac12338d1d472a5bc701c9994ba25
faik-binaj <faik.binaj@gmail.com>
2025-05-11 00:32:39 +0200

    Add ActiveUser decorator and interface for user data access
    Introduced the ActiveUser decorator to extract and manage authenticated user data within controllers. Updated the CoffeesController to demonstrate its usage and extended the JWT payload with a typed ActiveUserData interface for consistency.

--- src/iam/authentication/authentication.service.ts
+++ src/iam/authentication/authentication.service.ts
@@ -13,6 +13,7 @@ import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigType } from '@nestjs/config';
 import jwtConfig from '../config/jwt.config';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
 
 @Injectable()
 export class AuthenticationService {
@@ -65,7 +66,7 @@ export class AuthenticationService {
       {
         sub: user.id,
         email: user.email,
-      },
+      } as ActiveUserData,
       {
         audience: this.jwtConfiguration.audience,
         issuer: this.jwtConfiguration.issuer,
