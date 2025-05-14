2901c7280105199d0d892bd384a1f61cb88080a6
faik-binaj <faik.binaj@gmail.com>
2025-05-15 00:12:47 +0200

    Add Redis-based refresh token storage for better control
    Implemented `RefreshTokenIdsStorage` leveraging Redis for managing refresh tokens, enhancing token validation and revocation processes. Updated `AuthenticationService` to integrate this storage, ensuring invalid tokens are flagged and revoked securely. Added Redis service to `docker-compose` and updated dependencies to include `ioredis`.

---
+++ src/iam/authentication/refresh-token-ids.storage/refresh-token-ids.storage.ts
@@ -0,0 +1,49 @@
+import {
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+} from '@nestjs/common';
+import Redis from 'ioredis';
+
+export class InvalidRefreshTokenError extends Error {}
+
+@Injectable()
+export class RefreshTokenIdsStorage
+  implements OnApplicationBootstrap, OnApplicationShutdown
+{
+  private redisClient: Redis;
+
+  onApplicationBootstrap(): any {
+    // TODO: Ideally, we should move this to the dedicated "RedisModule"
+    // Instead of initializing the connection here.
+    this.redisClient = new Redis({
+      host: 'localhost', // NOTE: According to best practices, we should use the environment variables here instead
+      port: 6279,
+    });
+  }
+
+  onApplicationShutdown(signal?: string): any {
+    return this.redisClient.quit();
+  }
+
+  async insert(userId: number, tokenId: string): Promise<void> {
+    await this.redisClient.set(this.getKey(userId), tokenId);
+  }
+
+  async validate(userId: number, tokenId: string): Promise<boolean> {
+    const storedId = await this.redisClient.get(this.getKey(userId));
+
+    if (storedId !== tokenId) {
+      throw new InvalidRefreshTokenError();
+    }
+    return storedId === tokenId;
+  }
+
+  async invalidate(userId: number, tokenId: string): Promise<void> {
+    await this.redisClient.del(this.getKey(userId));
+  }
+
+  private getKey(userId: number): string {
+    return `user-${userId}`;
+  }
+}
